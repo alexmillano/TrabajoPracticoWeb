e1ab33c99c4f0d542db6411635e5a17d10665764
alex.millano <alexander.millano@davinci.edu.ar>
2024-04-28 18:23:02 -0300

    Cree Aldeano y Pesquero

--- aldeano.php
+++ aldeano.php
@@ -1,23 +1,52 @@
 <?php
     class Aldeano {
-
-        function __construct($velocidadRecoleccion='18'){
+        private $velocidadRecoleccion;
+        function __construct($velocidadRecoleccion= 18){
             $this->velocidadRecoleccion = $velocidadRecoleccion;
         }
         public function Recolectar(Arbusto $arbusto){
-            $tiempo = ceil($arbusto->getCantidadAlimento() / $this->velocidadRecoleccion);
+            $tiempo = ceil($arbusto->getAlimento() / $this->velocidadRecoleccion);
+            echo 'Se ha tardado ' .$tiempo. ' en Recolectar todo el alimento';
         }
     }
 
     class Arbusto {
         private $cantidadAlimento;
-        public function __construct() {
-            $this->cantidadAlimento = 125;
+        public function __construct($cantidadAlimento= 125) {
+            $this->cantidadAlimento = $cantidadAlimento;
         }
     
         public function getAlimento() {
-            return $this->cantidadAlimento;
+           return $this->cantidadAlimento;
+            
         }
     }
+$aldeano = new Aldeano();
+$arbusto = new Arbusto();
+$aldeano->Recolectar($arbusto);
 
+    class Pesquero {
+        private $velocidadRecoleccion;
+        function __construct($velocidadRecoleccion= 18){
+            $this->velocidadRecoleccion = $velocidadRecoleccion;
+        }
+        public function Recolectar(BancoDePesca $banco){
+            $tiempo = ceil($banco->getAlimento() / $this->velocidadRecoleccion);
+            echo '<br>Se ha tardado ' .$tiempo. ' en Recolectar todo el alimento';
+        }
+    }
+    class BancoDePesca {
+        private $cantidadAlimento;
+        public function __construct($cantidadAlimento= 225) {
+            $this->cantidadAlimento = $cantidadAlimento;
+        }
+
+        public function getAlimento() {
+        return $this->cantidadAlimento;
+            
+        }
+    }
+$pesquero = new Pesquero();
+$banco = new BancoDePesca();
+$pesquero->Recolectar($banco);
 ?>
