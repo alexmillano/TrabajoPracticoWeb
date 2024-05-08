337d5144a274d8c5870697ef85315d665d5c851e
alexmillano <159828484+alexmillano@users.noreply.github.com>
2024-05-08 09:39:40 -0300

    Se agrego el meta al codigo

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./diseño/estilo.css">
     <title>Trabajo practico 1</title>
+    <meta name="description" content="Pagina que contiene información de aldeano y pesquero, en cuanto al tiempo de recoleccion se alimento">
 </head>
 <body class="fondo">
     <header>
